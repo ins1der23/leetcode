b139ee7a4b6393f34e540acb96453a4a97b0c09a
ins1der23 <m.zadorin@uopt.ru>
2023-03-23 23:32:56 +0500

    StepsToZero done

---
+++ StepsToZero/Program.cs
@@ -0,0 +1,17 @@
+﻿int NumberOfSteps(int num)
+{
+    int count = 0;
+    if (num == 0) return count;
+    while (num > 1)
+    {
+        if (num % 2 == 1)
+        {
+            num--;
+            count++;
+        }
+        num = num / 2;
+        count++;
+    }
+    return count + 1;
+}
+Console.WriteLine(NumberOfSteps(0));
