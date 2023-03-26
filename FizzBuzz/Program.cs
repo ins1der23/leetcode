55290148d18d6fe1183859b0dd66dfdd0456f8ef
ins1der23 <m.zadorin@uopt.ru>
2023-03-27 00:31:04 +0500

    FizzBuzz solved recursively

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -1,21 +1,24 @@
-﻿// IList<string> FizzBuzz(int n)
-// {
-string[] someList = new string[15];
-for (int i = 0; i < 15; i++)
+﻿IList<string> FizzBuzz(int n)
 {
-    if ((i + 1) % 5 == 0 && (i + 1) % 3 == 0) someList[i] = "FizzBuzz ";
+string[] someList = new string[10000];
+int i = someList.Length - 1;
+
+void FizzBuzzRec(string[] stringArray, int i = 0)
+{
+    if (i < 0) return;
+    if ((i + 1) % 5 == 0 && (i + 1) % 3 == 0) stringArray[i] = "FizzBuzz";
     else
     {
-        if ((i + 1) % 5 == 0) someList[i] = "Buzz ";
-        else if ((i + 1) % 3 == 0) someList[i] = "Fizz ";
-        else  someList[i] = (i + 1).ToString();
-    } 
-        
+        if ((i + 1) % 5 == 0) stringArray[i] = "Buzz";
+        else if ((i + 1) % 3 == 0) stringArray[i] = "Fizz";
+        else stringArray[i] = (i + 1).ToString();
+    }
+    FizzBuzzRec(stringArray, i - 1);
 }
-foreach (var item in someList)
-{
-    Console.Write(item);
+FizzBuzzRec(someList, i);
+return someList;
 }
 
+IList<string> lst1 = FizzBuzz(20);
 
-// }
+Console.WriteLine(lst1[19]);
