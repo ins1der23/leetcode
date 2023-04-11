1242630f413ad5e8cf31a005d665b762f026584a
ins1der23 <m.zadorin@uopt.ru>
2023-04-11 12:42:03 +0500

    KWeakest added good decision

--- KWeakestRow/Program.cs
+++ KWeakestRow/Program.cs
@@ -41,18 +41,14 @@ int[] KWeakestRows(int[][] mat, int k)
         sumOfRow[i] = j;
     }
     var weakest = new List<int>();
-    int pivot = 0;
-    int min = 0;
+    int maxI = 0;
     for (int p = 0; p < sumsLength; p++)
     {
-        if (sumOfRow[p] > sumOfRow[pivot]) pivot = p;
+        if (sumOfRow[p] > sumOfRow[maxI]) maxI = p;
     }
     for (int i = 0; i < k; i++)
     {
-        for (int p = 0; p < sumsLength; p++)
-        {
-            if (sumOfRow[p] > sumOfRow[pivot]) pivot = p;
-        }
+        int pivot = maxI;
         for (int j = sumsLength - 1; j >= 0; j--)
         {
             if (!weakest.Contains(j) && sumOfRow[j] < (sumOfRow[pivot] + 1))
@@ -64,14 +60,43 @@ int[] KWeakestRows(int[][] mat, int k)
     return weakestArray;
 }
 
-int[][] someArray = CreateArray(7, 3);
+int[] KWeakestRows1(int[][] mat, int k)
+{
+    // Create a list of tuples where each tuple contains the number of soldiers and the index of the row
+    List<(int soldiers, int index)> rows = new List<(int, int)>();
+    for (int i = 0; i < mat.Length; i++)
+    {
+        int soldiers = 0;
+        for (int j = 0; j < mat[i].Length; j++)
+        {
+            soldiers += mat[i][j];
+        }
+        rows.Add((soldiers, i));
+    }
+    // Sort the list of tuples by the number of soldiers (ascending) and then by the index of the row (ascending)
+    rows.Sort((x, y) => x.soldiers == y.soldiers ? x.index - y.index : x.soldiers - y.soldiers);
+    // Create an array with the indices of the k weakest rows
+    int[] result = new int[k];
+    for (int i = 0; i < k; i++)
+    {
+        result[i] = rows[i].index;
+    }
+    return result;
+}
+
+int[][] someArray = CreateArray(10000, 10000);
 Stopwatch sw = new Stopwatch();
-sw.Start();
 FillArrayRandomIter(someArray);
-ArrayToConsole(someArray);
+// ArrayToConsole(someArray);
+sw.Start();
 var result = KWeakestRows(someArray, 6);
-Console.WriteLine();
-Console.WriteLine(String.Join(" ", result));
+// Console.WriteLine();
+// Console.WriteLine(String.Join(" ", result));
+sw.Stop();
+Console.WriteLine($"{sw.ElapsedMilliseconds}");
+sw.Reset();
+sw.Start();
+var result1 = KWeakestRows1(someArray, 6);
 sw.Stop();
 Console.WriteLine($"{sw.ElapsedMilliseconds}");
 
